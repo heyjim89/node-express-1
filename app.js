31edd70edb3e36aec05372706cdc448e0fa0d59f
Jim D <heyjim89@gmail.com>
2015-07-21 09:31:37 -0600

    Adding about page

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ var app = express();
 
 // app.set sets config settings (run once each on start of app)
 
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');  //tell express we're using jade
+app.set('views', __dirname + '/views'); // location of where jade files are
 
+//setting up for http request
 app.get('/', function(req, res) {
 	res.render('index');
+});
+
+app.get('/about', function(req,res){
+	res.render('about');
 });
 
 var server = app.listen(9996, function() {
